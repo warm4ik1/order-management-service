6805f277b072b3ed4a652c5880dba8e93a1da96c
warm4ik <kogbbccv@gmail.com>
2026-02-04 21:49:20 +0300

    Initial project setup: packages, application.yml, Liquibase migrations

---
+++ src/main/java/org/warm4ik/hub/oms/OrderManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package org.warm4ik.hub.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrderManagementServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/org/warm4ik/hub/oms/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.warm4ik.hub.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
