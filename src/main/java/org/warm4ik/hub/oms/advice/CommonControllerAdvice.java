562a67bab615a0465bf617d81f5fb45b065a57ca
warm4ik <kogbbccv@gmail.com>
2026-02-05 04:22:18 +0300

    Add custom exception + controllerAdvice + exception-constants

---
+++ src/main/java/org/warm4ik/hub/oms/advice/CommonControllerAdvice.java
@@ -0,0 +1,51 @@
+package org.warm4ik.hub.oms.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.warm4ik.hub.oms.model.constants.ApiConstants;
+import org.warm4ik.hub.oms.model.exception.NotFoundException;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Slf4j
+@ControllerAdvice
+public class CommonControllerAdvice {
+
+  @ExceptionHandler
+  @ResponseBody
+  protected ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+    logStackTrace(ex);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  private void logStackTrace(Exception ex) {
+    StringBuilder stackTrace = new StringBuilder();
+
+    stackTrace.append(ApiConstants.ANSI_RED);
+
+    stackTrace.append(ex.getMessage()).append(ApiConstants.BREAK_LINE);
+
+    if (Objects.nonNull(ex.getCause())) {
+      stackTrace.append(ex.getCause().getMessage()).append(ApiConstants.BREAK_LINE);
+    }
+
+    Arrays.stream(ex.getStackTrace())
+        .filter(st -> st.getClassName().startsWith(ApiConstants.TIME_ZONE_PACKAGE_NAME))
+        .forEach(
+            st ->
+                stackTrace
+                    .append(st.getClassName())
+                    .append(".")
+                    .append(st.getMethodName())
+                    .append(" (")
+                    .append(st.getLineNumber())
+                    .append(") "));
+
+    log.error(stackTrace.append(ApiConstants.ANSI_WHITE).toString());
+  }
+}
---
+++ src/main/java/org/warm4ik/hub/oms/model/constants/ApiErrorMessage.java
@@ -0,0 +1,18 @@
+package org.warm4ik.hub.oms.model.constants;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ApiErrorMessage {
+    USER_NOT_FOUND_BY_ID("User with ID: %s was not found"),
+    ORDER_NOT_FOUND_BY_ID("Order with ID: %s was not found");
+
+    private final String message;
+
+    public String getMessage(Object... args) {
+        return String.format(message, args);
+    }
+}
