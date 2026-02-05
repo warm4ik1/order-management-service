3149f44976855d0d3c0565d581e039660a12dc7a
warm4ik <kogbbccv@gmail.com>
2026-02-05 13:31:28 +0300

    Added necessary libraries in build.gradle

--- src/main/java/org/warm4ik/hub/oms/model/response/ApiResponse.java
+++ src/main/java/org/warm4ik/hub/oms/model/response/ApiResponse.java
@@ -13,19 +13,20 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse<P extends Serializable> implements Serializable {
-    private String message;
-    private P payload;
-    private boolean success;
+  private String message;
+  private P payload;
+  private boolean success;
 
-    public static <P extends Serializable> ApiResponse<P> createSuccessful(P payload) {
-        return new ApiResponse<>(StringUtils.EMPTY, payload, true);
-    }
+  public static <P extends Serializable> ApiResponse<P> createSuccessful(P payload) {
+    return new ApiResponse<>(StringUtils.EMPTY, payload, true);
+  }
 
-    public static <P extends Serializable> ApiResponse<P> createSuccessful(String message, P payload) {
-        return new ApiResponse<>(message, payload, true);
-    }
+  public static <P extends Serializable> ApiResponse<P> createSuccessful(
+      String message, P payload) {
+    return new ApiResponse<>(message, payload, true);
+  }
 
-    public static <P extends Serializable> ApiResponse<P> createFailed(String message) {
-        return new ApiResponse<>(message, null, false);
-    }
+  public static <P extends Serializable> ApiResponse<P> createFailed(String message) {
+    return new ApiResponse<>(message, null, false);
+  }
 }
