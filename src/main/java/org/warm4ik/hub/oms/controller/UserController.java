323198cbe4e414bed2a8996b4db538eeacc63e4c
warm4ik <kogbbccv@gmail.com>
2026-02-06 18:20:00 +0300

    Added getAllUsers() and deleteUserById() api without security

--- src/main/java/org/warm4ik/hub/oms/controller/UserController.java
+++ src/main/java/org/warm4ik/hub/oms/controller/UserController.java
@@ -2,16 +2,25 @@ package org.warm4ik.hub.oms.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.warm4ik.hub.oms.model.dto.UserDTO;
 import org.warm4ik.hub.oms.model.request.user.RegisterUserRequest;
 import org.warm4ik.hub.oms.model.response.ApiResponse;
+import org.warm4ik.hub.oms.model.response.PaginationResponse;
 import org.warm4ik.hub.oms.service.UserService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${end.point.users}")
@@ -25,5 +34,24 @@ public class UserController {
       @RequestBody @Valid RegisterUserRequest request) {
 
     return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(request));
+  }
+
+  @GetMapping()
+  public ResponseEntity<ApiResponse<PaginationResponse<UserDTO>>> findAllUsers(
+      @RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "limit", defaultValue = "10") int limit) {
+
+    Pageable pageable = PageRequest.of(page, limit);
+    ApiResponse<PaginationResponse<UserDTO>> response = userService.findAllUsers(pageable);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteUserById(@PathVariable(name = "id") UUID id) {
+
+    userService.deleteUserById(id);
+
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/org/warm4ik/hub/oms/service/UserService.java
+++ src/main/java/org/warm4ik/hub/oms/service/UserService.java
@@ -1,13 +1,20 @@
 package org.warm4ik.hub.oms.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.warm4ik.hub.oms.mapper.UserMapper;
 import org.warm4ik.hub.oms.model.dto.UserDTO;
 import org.warm4ik.hub.oms.model.entity.User;
+import org.warm4ik.hub.oms.model.exception.NotFoundException;
 import org.warm4ik.hub.oms.model.request.user.RegisterUserRequest;
 import org.warm4ik.hub.oms.model.response.ApiResponse;
+import org.warm4ik.hub.oms.model.response.PaginationResponse;
 import org.warm4ik.hub.oms.repository.UserRepository;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +23,7 @@ public class UserService {
   private final UserRepository userRepository;
   private final UserMapper userMapper;
 
+  @Transactional
   public ApiResponse<UserDTO> register(RegisterUserRequest request) {
 
     User user = userMapper.createUser(request);
@@ -23,5 +31,32 @@ public class UserService {
 
     return ApiResponse.createSuccessful(
         "Регистрация прошла успешно!", userMapper.userToUserDTO(user));
+  }
+
+  public ApiResponse<PaginationResponse<UserDTO>> findAllUsers(Pageable pageable) {
+
+    Page<UserDTO> users = userRepository.findAll(pageable).map(userMapper::userToUserDTO);
+
+    PaginationResponse<UserDTO> paginationResponse =
+        new PaginationResponse<>(
+            users.getContent(),
+            new PaginationResponse.Pagination(
+                users.getTotalElements(),
+                pageable.getPageSize(),
+                pageable.getPageNumber() + 1,
+                users.getTotalPages()));
+
+    return ApiResponse.createSuccessful(
+        "Все доступные пользователи успешно получены.", paginationResponse);
+  }
+
+  @Transactional
+  public void deleteUserById(UUID id) {
+
+    if (!userRepository.existsById(id)) {
+      throw new NotFoundException("User с id " + id + " не найден и не может быть удалён.");
+    }
+
+    userRepository.deleteById(id);
   }
 }
