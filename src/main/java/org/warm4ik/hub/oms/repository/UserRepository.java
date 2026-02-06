63dc39faeba907b838712c3fa61249f62feeb3b2
warm4ik <kogbbccv@gmail.com>
2026-02-07 02:13:01 +0300

    Added user authentication entities and service, +findByUsername() in userRepository

--- src/main/java/org/warm4ik/hub/oms/repository/UserRepository.java
+++ src/main/java/org/warm4ik/hub/oms/repository/UserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.warm4ik.hub.oms.model.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   boolean existsByUsername(@NotBlank @Size(min = 8, max = 100) String username);
+
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/org/warm4ik/hub/oms/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package org.warm4ik.hub.oms.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.warm4ik.hub.oms.model.entity.User;
+import org.warm4ik.hub.oms.model.enums.UserRole;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NullMarked
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+  private final UUID id;
+  private final String username;
+  private final @Nullable String password;
+  private final UserRole role;
+  private final List<GrantedAuthority> authorities;
+
+  public static UserDetails fromEntity(User user) {
+    return new CustomUserDetails(
+        user.getId(),
+        user.getUsername(),
+        user.getPassword(),
+        user.getRole(),
+        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())));
+  }
+
+  public static CustomUserDetails fromJwt(UUID id, String username, UserRole role) {
+    return new CustomUserDetails(
+        id, username, null, role, List.of(new SimpleGrantedAuthority("ROLE_" + role.name())));
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public @Nullable String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+}
