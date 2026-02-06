5ea22f8aca0ce39cc00ed408453c2ec6c2221cd8
warm4ik <kogbbccv@gmail.com>
2026-02-06 19:16:50 +0300

    Add username uniqueness validation in user registration

--- src/main/java/org/warm4ik/hub/oms/repository/UserRepository.java
+++ src/main/java/org/warm4ik/hub/oms/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.warm4ik.hub.oms.repository;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.warm4ik.hub.oms.model.entity.User;
@@ -9,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+  boolean existsByUsername(@NotBlank @Size(min = 8, max = 100) String username);
 }
--- src/main/java/org/warm4ik/hub/oms/service/AuthService.java
+++ src/main/java/org/warm4ik/hub/oms/service/AuthService.java
@@ -5,8 +5,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.warm4ik.hub.oms.mapper.UserMapper;
+import org.warm4ik.hub.oms.model.constants.ApiErrorMessage;
 import org.warm4ik.hub.oms.model.dto.UserDTO;
 import org.warm4ik.hub.oms.model.entity.User;
+import org.warm4ik.hub.oms.model.exception.DataExistException;
 import org.warm4ik.hub.oms.model.request.user.RegisterUserRequest;
 import org.warm4ik.hub.oms.model.response.ApiResponse;
 import org.warm4ik.hub.oms.repository.UserRepository;
@@ -22,11 +24,17 @@ public class AuthService {
   @Transactional
   public ApiResponse<UserDTO> register(RegisterUserRequest request) {
 
+    if (userRepository.existsByUsername(request.getUsername())) {
+      throw new DataExistException(
+          ApiErrorMessage.USERNAME_ALREADY_EXISTS.getMessage(request.getUsername()));
+    }
+
     User user = userMapper.createUser(request);
     user.setPassword(passwordEncoder.encode(request.getPassword()));
     userRepository.save(user);
+    UserDTO userDTO = userMapper.userToUserDTO(user);
 
     return ApiResponse.createSuccessful(
-        "Регистрация прошла успешно!", userMapper.userToUserDTO(user));
+        "Регистрация прошла успешно!", userDTO);
   }
 }
