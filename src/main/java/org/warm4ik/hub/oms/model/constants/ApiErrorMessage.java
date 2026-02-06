c02e675265704ab9db51baaab413030e9f4abb65
warm4ik <kogbbccv@gmail.com>
2026-02-06 23:13:04 +0300

    Added new constant in ApiErrorMessage

--- src/main/java/org/warm4ik/hub/oms/model/constants/ApiErrorMessage.java
+++ src/main/java/org/warm4ik/hub/oms/model/constants/ApiErrorMessage.java
@@ -8,9 +8,16 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ApiErrorMessage {
     USER_NOT_FOUND_BY_ID("User with ID: %s was not found."),
+    USER_NOT_FOUND_BY_NAME("User: '%s' was not found."),
     ORDER_NOT_FOUND_BY_ID("Order with ID: %s was not found."),
     USERNAME_ALREADY_EXISTS("Username: %s already exists."),
-    ORDER_ALREADY_COMPLETED("The order has already been completed! The status cannot be changed.");
+    ORDER_ALREADY_COMPLETED("The order has already been completed! The status cannot be changed."),
+
+    INVALID_TOKEN_SIGNATURE("Invalid token signature"),
+    ERROR_DURING_JWT_PROCESSING("An unexpected error occurred during JWT processing"),
+    TOKEN_EXPIRED("Token expired."),
+    UNEXPECTED_ERROR_OCCURRED("An unexpected error occurred. Please try again later.");
+
 
     private final String message;
 
