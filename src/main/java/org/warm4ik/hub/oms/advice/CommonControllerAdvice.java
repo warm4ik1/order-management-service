e341f6d00eee10237c0fdaf8060c84cc0c6bc9da
warm4ik <kogbbccv@gmail.com>
2026-02-07 20:02:54 +0300

    Handle new exception (global handler)

--- src/main/java/org/warm4ik/hub/oms/advice/CommonControllerAdvice.java
+++ src/main/java/org/warm4ik/hub/oms/advice/CommonControllerAdvice.java
@@ -3,14 +3,19 @@ package org.warm4ik.hub.oms.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.warm4ik.hub.oms.model.constants.ApiConstants;
+import org.warm4ik.hub.oms.model.exception.BusinessConflictException;
 import org.warm4ik.hub.oms.model.exception.DataExistException;
 import org.warm4ik.hub.oms.model.exception.NotFoundException;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -24,15 +29,42 @@ public class CommonControllerAdvice {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 
+  @ExceptionHandler
+  @ResponseBody
+  protected ResponseEntity<String> handleBusinessConflictException(BusinessConflictException ex) {
+    logStackTrace(ex);
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  @ResponseBody
+  protected ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+    logStackTrace(ex);
+    return ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
+            .body(ex.getMessage());
+  }
+
   @ExceptionHandler(DataExistException.class)
   @ResponseBody
   protected ResponseEntity<String> handleDataExistException(DataExistException ex) {
     logStackTrace(ex);
 
-    return ResponseEntity
-            .status(HttpStatus.CONFLICT)
-            .body(ex.getMessage());
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseBody
+  protected ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    logStackTrace(ex);
+
+    List<String> errors = ex.getBindingResult().getAllErrors().stream()
+            .map(ObjectError::getDefaultMessage)
+            .toList();
+
+    return ResponseEntity.badRequest().body(errors);
+  }
+
 
   private void logStackTrace(Exception ex) {
     StringBuilder stackTrace = new StringBuilder();
