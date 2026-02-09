15c14c134ce8a3705e318571e7350268fcc97242
warm4ik <kogbbccv@gmail.com>
2026-02-09 13:49:37 +0300

    Added @Schema in request layers

--- src/main/java/org/warm4ik/oms/model/request/auth/LoginRequest.java
+++ src/main/java/org/warm4ik/oms/model/request/auth/LoginRequest.java
@@ -1,5 +1,6 @@
 package org.warm4ik.oms.model.request.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,17 @@ public class LoginRequest {
 
   @NotBlank(message = "Username is required")
   @Size(min = 8, max = 100, message = "Username must be between 8 and 100 characters")
+  @Schema(
+      description = "Unique username (from 8 to 100 symbols)",
+      example = "alexandra@mail.ru",
+      requiredMode = Schema.RequiredMode.REQUIRED)
   String username;
 
   @NotBlank(message = "Password is required")
   @Size(min = 5, max = 50, message = "Password must be between 5 and 50 characters")
+  @Schema(
+      description = "User password (from 5 to 50 symbols)",
+      example = "password123",
+      requiredMode = Schema.RequiredMode.REQUIRED)
   String password;
 }
--- src/main/java/org/warm4ik/oms/model/request/order/UpdateStatusOrderRequest.java
+++ src/main/java/org/warm4ik/oms/model/request/order/UpdateStatusOrderRequest.java
@@ -1,5 +1,6 @@
 package org.warm4ik.oms.model.request.order;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +13,10 @@ import org.warm4ik.oms.model.enums.OrderStatus;
 @NoArgsConstructor
 public class UpdateStatusOrderRequest {
 
-    @NotNull(message = "Status is required")
-    private OrderStatus status;
+  @NotNull(message = "Status is required")
+  @Schema(
+      description = "OrderStatus: CREATED, IN_PROGRESS, COMPLETED",
+      example = "IN_PROGRESS",
+      requiredMode = Schema.RequiredMode.REQUIRED)
+  private OrderStatus status;
 }
